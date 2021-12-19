22446ceb434a71d7d9d54fb15323270daaba9fa5
developerkhashab <mohammad.khashab.mk@gmail.com>
2021-12-20 00:01:42 +0200

    Add third party controller

---
+++ Controllers/ThirdPartyController.cs
@@ -0,0 +1,37 @@
+﻿using IBKS.Context;
+using IBKS.Context.Entities;
+using IBKS.DataAccess;
+using IBKS.Managers.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IBKS.Controllers
+{
+    public class ThirdPartyController : Controller
+    {
+        private readonly DBContext _context;
+        public ThirdPartyController(DBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Posts")]
+        public virtual List<Post> GetALLPosts()
+        {
+            return _context.Posts.ToList();
+        }
+
+        [HttpGet("Users")]
+        public virtual List<User> GetALLUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        [HttpGet("Comments")]
+        public virtual List<Comment> GetALLComments()
+        {
+            return _context.Comments.ToList();
+        }
+    }
+}
